a7657964dfac72cdd64ffbb5a72c59c5cc78ad1b
Parrot1999 <parrot14996@gmail.com>
2021-10-27 20:03:48 +0500

    push

---
+++ build/precache-manifest.45af8ac5b3217d03b90200aa30c814d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cddd6ef6462affccc5e60a85ba9f0aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a87962968bfe2d8de95",
+    "url": "/static/css/main.520cfd00.chunk.css"
+  },
+  {
+    "revision": "b87eae66872fe7698b24",
+    "url": "/static/js/2.74625c8b.chunk.js"
+  },
+  {
+    "revision": "1a87962968bfe2d8de95",
+    "url": "/static/js/main.a1b3ae3e.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
